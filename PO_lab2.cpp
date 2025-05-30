0f545b041edf3789a95e1f552c38cccf585a36c4
NEPAMPUHI <140514246+NEPAMPUHI@users.noreply.github.com>
2025-05-30 16:22:28 +0300

    added solution without parallelization

--- PO_lab2.cpp
+++ PO_lab2.cpp
@@ -1,20 +1,60 @@
-// PO_lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Find the number of elements greater than 20, as well as the largest such number.
 
 #include <iostream>
+#include <chrono>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+using namespace chrono;
+
+static const int threads_num = 2;
+static const int arr_size = 2000;
+int arr[arr_size];
+
+void fill_array() {
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = rand() % 30 + 1;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void solution_without_parallelization(int& number, int& largest) {
+    int counter = 0;
+    int max = 0;
+    for (int i = 0; i < arr_size; i++) {
+        if (arr[i] > 20) {
+            counter++;
+            if (arr[i] > max)
+                max = arr[i];
+        }
+    }
+    number = counter;
+    largest = max;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool is_correct(int n, int l) {
+    int number;
+    int largest;
+    solution_without_parallelization(number, largest);
+    return number == n && largest == l;
+}
+
+void print_results(const string& solution_type, double duration, int n, int l) {
+    cout << solution_type << ": " << duration << " seconds" << endl;
+    cout << "The number = " << n << ", the largest = " << l << endl;
+    cout << "Matrix is " << (is_correct(n, l) ? "" : "NOT ") << "correct" << endl << endl;
+}
+
+int main() {
+    srand(123456789);
+    fill_array();
+
+    int number;
+    int largest;
+
+    auto begin = high_resolution_clock::now();
+    solution_without_parallelization(number, largest);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(end - begin).count() * 1e-9;
+    print_results("Solution without parallelization", duration, number, largest);
+
+    return 0;
+}
